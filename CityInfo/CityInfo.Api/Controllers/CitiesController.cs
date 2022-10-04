ce255bca62e70bd7b0050979bd4c4a9ba745ef03
stepanbacho <78855270+stepanbacho@users.noreply.github.com>
2022-10-04 23:42:34 +0300

    updated

---
+++ CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id =1, Name = "New York City"},
+                    new {id = 2, Name = "Antwerp"}
+                });
+        }
+    }
+}
